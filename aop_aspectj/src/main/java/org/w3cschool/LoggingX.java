5f573fce863189e070d6f663d4a9e412552dc768
caoliang <caoliang@jiangduoduo.com>
2020-03-30 18:39:18 +0800

    xml配置aspect

--- aop_aspectj/src/main/java/org/w3cschool/LoggingX.java
+++ aop_aspectj/src/main/java/org/w3cschool/LoggingX.java
@@ -1,33 +1,21 @@
 package org.w3cschool;
 
-import org.aspectj.lang.annotation.*;
-import org.springframework.stereotype.Component;
-
 /**
- * 注解配置
+ * xml配置
  */
-@Aspect
-@Component
-public class Logging {
-    @Pointcut("execution(* org.w3cschool.*.*(..))")
-    private void selectAll(){}
-
-    @Before("selectAll()")
+public class LoggingX {
     public void beforeAdvice(){
         System.out.println("Going to setup student profile.");
     }
 
-    @After("selectAll()")
     public void afterAdvice(){
         System.out.println("Student profile has been setup.");
     }
 
-    @AfterReturning(pointcut = "selectAll()", returning="retVal")
     public void afterReturningAdvice(Object retVal){
         System.out.println("Returning:" + retVal.toString() );
     }
 
-    @AfterThrowing(pointcut = "selectAll()", throwing = "ex")
     public void afterThrowingAdvice(IllegalArgumentException ex){
         System.out.println("There has been an exception: " + ex.toString());
     }
