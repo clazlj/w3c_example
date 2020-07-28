5da9d0f14f5d9b2e3a2076b4dd6198fd3728b07c
caoliang <caoliang@jiangduoduo.com>
2020-07-28 16:00:50 +0800

    add SpEL demo

--- spring_demo/src/main/java/org/w3cschool/spring/spel/DemoSpEL.java
+++ spring_demo/src/main/java/org/w3cschool/spring/spel/DemoSpEL.java
@@ -27,5 +27,22 @@ public class DemoSpEL {
         //Inline Maps
         Map inventorInfo = (Map) parser.parseExpression("{name:'Nikola',dob:'10-July-1856'}").getValue(context);
         Map mapOfMaps = (Map) parser.parseExpression("{name:{first:'Nikola',last:'Tesla'},dob:{day:10,month:'July',year:1856}}").getValue(context);
+
+        //Array Construction
+        int[] numbers1 = (int[]) parser.parseExpression("new int[4]").getValue(context);
+        int[] numbers2 = (int[]) parser.parseExpression("new int[]{1,2,3}").getValue(context);
+        int[][] numbers3 = (int[][]) parser.parseExpression("new int[4][5]").getValue(context);
+
+        //Methods
+        String bc = parser.parseExpression("'abc'.substring(1,3)").getValue(String.class);
+
+        //Operators
+        Boolean trueValue1 = parser.parseExpression("2 == 2").getValue(Boolean.class);
+        Boolean falseValue1 = parser.parseExpression("2 < -5.0").getValue(Boolean.class);
+        Boolean trueValue2 = parser.parseExpression("'black' < 'block'").getValue(Boolean.class);
+        boolean falseValue2 = parser.parseExpression("'xyz' instanceof T(Integer)").getValue(Boolean.class);
+        boolean trueValue3 = parser.parseExpression("'5.00' matches '^-?\\d+(\\.\\d{2})?$'").getValue(Boolean.class);
+        boolean falseValue3 = parser.parseExpression("'5.0067' matches '^-?\\d+(\\.\\d{2})?$'").getValue(Boolean.class);
+
     }
 }
